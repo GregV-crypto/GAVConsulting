414d5cd9db341a376aa5d827bf6c8823a48a4153
Greg Vincent <gavincent@albany.edu>
2023-05-25 19:43:35 -0400

    Initial Code Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HomePage from "./components/HomePage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="content-container">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,34 @@
+import "./HomePage.css";
+function HomePage() {
+  return (
+    <div className="HP-container">
+      <h1>
+        G.A.V Consulting
+        <br></br>
+        <em>Assistance at the Crossroads of Business and Technology</em>
+      </h1>
+      <p>
+        G.A.V Consulting provides a variety of services necessary to improve our
+        client's business relative to their specific goals. Regardless of
+        whether said client is government-affiliated, a non-profit or
+        for-profit, we have the skills to help their organization achieve their
+        goals - financial, performance-based, or otherwised stated. This
+        statement is backed by a 17-year long track record in this field
+        improving the overall well-being of companies across New York State. Our
+        services include but aren't limited to the following.
+      </p>
+      <ul>
+        <li>Business Process Management and Consultation</li>
+        <li>Information Technology Planning</li>
+        <li>Strategic Plan Development</li>
+        <li>Cash Flow and Inventory Analysis</li>
+        <li>Marketing Plan Development</li>
+        <li>Technology Procurement</li>
+        <li>GAP Analysis</li>
+        <li>General Project Management</li>
+        <li>Process Re-Engineering</li>
+      </ul>
+    </div>
+  );
+}
+export default HomePage;
